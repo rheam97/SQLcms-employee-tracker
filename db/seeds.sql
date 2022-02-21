1a34986eff41b84ea39588691c69e4ad58643c1d
Rhea <rheam97@gmail.com>
2022-02-20 20:04:08 -0500

    deleted draft queries from seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -42,30 +42,5 @@ VALUE ("Tom", "Nice", 2, 7);
 
 
 
-DELETE FROM employee WHERE employee_id=?
-DELETE FROM department WHERE department.department_id=?
-DELETE FROM role WHERE role.role_id=?
 
-UPDATE employee SET manager_id=? WHERE employee_id =?
-
-
-SELECT count(*) as staff_count, sum(role.salary) as budget, department.name as department 
-from employee
-left join role on employee.role_id=role.role_id
-left join department on role.department_id=department.department_id
-group by department
-
---you want to get all the employees that serve a role in a partivular dept
-SELECT employee.employee_id, employee.first_name, employee.last_name, department.name as department
-from employee
-LEFT JOIN role on employee.role_id=role.role_id
-left join department on role.department_id=department.department_id
-where department.department_id = ?
-
---get all managers from depts
-SELECT employee.first_name, employee.last_name, department.name as department
-from employee
-LEFT JOIN role on employee.role_id=role.role_id
-left join department on role.department_id=department.department_id
-where manager_id IS NOT NULL
 
